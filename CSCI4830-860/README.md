d5cb088b2e97e3078826dec0b9dc4f0a90d26c1a
Nathan <lyon.nathan@gmail.com>
2017-06-05 14:48:21 +0000

    done with fun_with_strings

---
+++ CSCI4830-860/README.md
@@ -0,0 +1,2 @@
+# CSCI4830-860
+Classwork Repository for Intro to Software Engineering
---
+++ CSCI4830-860/ruby-calisthenics/lib/fun_with_strings.rb
@@ -0,0 +1,53 @@
+module FunWithStrings
+  # palindrome?
+  def palindrome?
+    self.downcase!
+    self.gsub!(/[^a-zA-Z]/, "")
+	  self == self.reverse
+  end
+  # count_words
+  def count_words
+    sArr = self.split(" ")
+    sHsh = new.Hash
+	  sArr.each do |w|
+	    w.gsub!(/[^a-zA-Z]/, "")
+		  w.downcase!
+		  if sHsh[w] == nil
+			  sHsh[w] = 1
+		  else
+			  sHsh[w] += 1
+		  end
+		end
+		return sHsh
+  end
+  # anagram_groups
+  def anagram_groups
+    num = 0
+    sHsh = new.Hash
+    sArr = []
+    buffArr =[]
+    if s == nil || self == ""
+      return arr = []
+    else
+      arr = self.split(" ")
+      arr.each do |w|
+        w.gsub!(/[^a-zA-Z]/, "")
+        num = w.downcase.sum
+        sHsh[num.to_sym] = w
+      end  
+      sHsh.each do |key, value|
+        value.values.each do |v|
+          buffArr.push(v)
+        end
+        sArr.push(buffArr)
+      end
+      return sArr
+    end  
+  end
+end
+# make all the above functions available as instance methods on Strings:
+
+class String
+  include FunWithStrings
+end
+
